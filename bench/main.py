57422583655599488a2c3a74e29fd8eee0071e4f
premky85 <leon.premky@gmail.com>
2020-02-07 15:05:36 +0100

    misc

--- bench/main.py
+++ bench/main.py
@@ -5,6 +5,7 @@ import subprocess
 from ctypes import *
 import random
 import time
+import gc
 
 small = [x for x in range(100, 1000, 100)]
 medium = [x for x in range(1000, 10000, 1000)]
@@ -20,9 +21,12 @@ dtw.dtw_diag.restype = c_double
 dtw.dtw_fwbk.restype = c_double
 dtw.dtw_fwbk_par.restype = c_double
 
-algs = [(dtw.dtw_fw, "dtw_fw"), (dtw.dtw_bk, "dtw_bk"),
-        (dtw.dtw_diag, "dtw_diag"), (dtw.dtw_fwbk, "dtw_fwbk"),
-        (dtw.dtw_fwbk_par, "dtw_fwbk_par"), (dtw.dtw_prunned, "dtw_prunned")] #(dtw.dtw_prunned, "dtw_prunned")
+algs = [(dtw.dtw_fw, "dtw_fw"),
+        (dtw.dtw_diag, "dtw_diag"),
+        (dtw.dtw_bk, "dtw_bk"),
+        (dtw.dtw_fwbk, "dtw_fwbk"),
+        (dtw.dtw_fwbk_par, "dtw_fwbk_par"),
+        (dtw.dtw_prunned, "dtw_prunned")]
 
 for alg in algs:
     f = open("results/" + alg[1] + ".csv", "w")
@@ -36,12 +40,15 @@ for x in small:
     arr1 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arr2 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arrt = np.zeros(x*x).ctypes.data_as(POINTER(c_double))
+    gc.collect()
+
     dtw.dtw_fw(arr1, arr2, arrt, x, x)
 
     for alg in algs:
         start = time.perf_counter()
-        print(alg[0](arr1, arr2, arrt, x, x))
+        rez = alg[0](arr1, arr2, arrt, x, x)
         end = time.perf_counter()
+        print(rez)
 
         f = open("results/" + alg[1] + ".csv", "a+")
         f.write(str(x) + "," + str(end-start) + "\n")
@@ -53,12 +60,15 @@ for x in medium:
     arr1 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arr2 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arrt = np.zeros(x * x).ctypes.data_as(POINTER(c_double))
+    gc.collect()
+
     dtw.dtw_fw(arr1, arr2, arrt, x, x)
 
     for alg in algs:
         start = time.perf_counter()
-        print(alg[0](arr1, arr2, arrt, x, x))
+        rez = alg[0](arr1, arr2, arrt, x, x)
         end = time.perf_counter()
+        print(rez)
 
         f = open("results/" + alg[1] + ".csv", "a+")
         f.write(str(x) + "," + str(end - start) + "\n")
@@ -70,12 +80,15 @@ for x in large:
     arr1 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arr2 = np.rint(np.random.rand(x) * 10).ctypes.data_as(POINTER(c_int))
     arrt = np.zeros(x * x).ctypes.data_as(POINTER(c_double))
+    gc.collect()
+
     dtw.dtw_fw(arr1, arr2, arrt, x, x)
 
     for alg in algs:
         start = time.perf_counter()
-        print(alg[0](arr1, arr2, arrt, x, x))
+        rez = alg[0](arr1, arr2, arrt, x, x)
         end = time.perf_counter()
+        print(rez)
 
         f = open("results/" + alg[1] + ".csv", "a+")
         f.write(str(x) + "," + str(end - start) + "\n")
--- bench/plot.py
+++ bench/plot.py
@@ -14,6 +14,7 @@ data.columns = ["time_fw", "time_bk", "time_diag", "time_fwbk", "time_fwbk_par",
 
 plt.figure()
 data.plot()
+plt.ylabel("sec")
 plt.show()
 
 
