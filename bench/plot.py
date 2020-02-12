610b9edc445a1bd52aca6a11e914cfb76aab4aa6
premky85 <leon.premky@gmail.com>
2020-02-12 09:34:20 +0100

    Added dtw_diag_par.h, must fix cache optimisation

--- bench/plot.py
+++ bench/plot.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 data_fw = pd.read_csv("results/dtw_fw.csv", index_col="size")
 data_bk = pd.read_csv("results/dtw_bk.csv", index_col="size")
 data_diag = pd.read_csv("results/dtw_diag.csv", index_col="size")
+data_diag_par = pd.read_csv("results/dtw_diag_par.csv", index_col="size")
 data_fwbk = pd.read_csv("results/dtw_fwbk.csv", index_col="size")
 data_fwbk_par = pd.read_csv("results/dtw_fwbk_par.csv", index_col="size")
 data_prunned = pd.read_csv("results/dtw_prunned.csv", index_col="size")
 
-data = pd.concat([data_fw, data_bk, data_diag, data_fwbk, data_fwbk_par, data_prunned], axis=1)
-data.columns = ["time_fw", "time_bk", "time_diag", "time_fwbk", "time_fwbk_par", "time_prunned"]
+data = pd.concat([data_fw, data_bk, data_diag, data_fwbk, data_fwbk_par, data_prunned, data_diag_par], axis=1)
+data.columns = ["time_fw", "time_bk", "time_diag", "time_fwbk", "time_fwbk_par", "time_prunned", "time_diag_par"]
 
 plt.figure()
 data.plot()
